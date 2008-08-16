766a7c3a1a8653857cdde0da48ef96e2ccfcb308
Maria & Nathan <nathansobo@gmail.com>
2008-08-16 13:47:20 -0700

    Starting to build recipe

--- lib/gallery/generator.rb
+++ lib/gallery/generator.rb
@@ -10,7 +10,36 @@ module Gallery
       dir = File.dirname(__FILE__)      
       template = ERB.new(File.read("#{dir}/client_index.erb"))
       template.result(binding)
+    end
+    
+    
+    
+
+
+
+    
+    def recipe
+      jobs = []
+      Dir.foreach(@path) do |subdir|
+        
+        match = subdir.match(/^([0-9]+)_([^_]+)_(.+)/)    
+        next unless match
+        
+        number = match[1]
+        name = match[2]
+        date = match[3]
+        
+        
+        jobs.push({
+          :date => date,
+          :number => number,
+          :href => subdir
+        })
+      end
       
+      {
+        :archives => jobs
+      }
     end
   end
 end
--- spec/gallery/generator_spec.rb
+++ spec/gallery/generator_spec.rb
@@ -1,34 +1,73 @@
 dir = File.dirname(__FILE__)
 require "#{dir}/../spec_helper"
 
-describe Gallery::Generator do
-  it "works" do
+describe Gallery::Generator do  
+  attr_reader :generator
+  before do
     fixtures_path = "#{dir}/../fixtures"
-    generator = Gallery::Generator.new(fixtures_path)
-        
-    puts generator.generate({
-      :current_job => {
-        :date => "7-31-08",
-        :number => 2392,
-        :href => "http://moose.com"
-      },      
-      :archives => [
-        {
-          :date => "5-30-08",
-          :number => 2391,
-          :href => "#"
-        },
-        {
-          :date => "3-15-08",
-          :number => 2390,
-          :href => "#"          
-        },
-        {
-          :date => "3-01-08",
-          :number => 2388,
-          :href => "#"          
-        }
-      ]
-    })
+    @generator = Gallery::Generator.new(fixtures_path)
   end
+  
+  describe "#generate" do
+    it "takes a recipe and returns an index page based on it" do
+      content = generator.generate({
+        :current_job => {
+          :date => "2008_07_31",
+          :number => 2392,
+          :href => "2392_Silverlign_2008_07_31"
+        },      
+        :archives => [
+          {
+            :date => "2008_06_15",
+            :number => 2391,
+            :href => "2391_Silverlign_2008_06_15"
+          },
+          {
+            :date => "2008_04_10",
+            :number => 2390,
+            :href => "2390_Silverlign_2008_04_10"
+          }
+        ]
+      })
+      
+      content.should match(/2392/)
+      content.should match(/2391/)
+      content.should match(/2390/)
+      
+      content.should match(/2008_07_31/)
+      content.should match(/2008_06_15/)
+      content.should match(/2008_04_10/)
+
+      content.should match(/2392_Silverlign_2008_07_31/)
+      content.should match(/2391_Silverlign_2008_06_15/)
+      content.should match(/2390_Silverlign_2008_04_10/)
+    end
+  end
+  
+  describe "#recipe" do
+    it "returns a recipe based on the target directory" do
+      generator.recipe.should == {
+        :archives => [
+          {
+            :date => "2008_04_10",
+            :number => "2390",
+            :href => "2390_Silverlign_2008_04_10"
+          },
+          {
+            :date => "2008_06_15",
+            :number => "2391",
+            :href => "2391_Silverlign_2008_06_15"
+          },
+          {
+            :date => "2008_07_31",
+            :number => "2392",
+            :href => "2392_Silverlign_2008_07_31"
+          }
+        ]
+      }
+    end
+    
+    
+  end
+  
 end
